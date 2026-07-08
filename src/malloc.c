6816b196bbc9d969d430ee33487ebf7c64e47939
smonsech <santiago@emeks.com.ar>
2026-07-08 17:55:05 -0300

    basic allocation with boundary tags mallco/free via sbrk (no coalescing yet)

--- src/malloc.c
+++ src/malloc.c
@@ -4,21 +4,25 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdint.h>
 
 const size_t PADDING_ALIGNMENT = 7;
 
-static inline size_t _get_block_size(size_t raw_size) { return raw_size + 2*sizeof(size_t) + ((raw_size + PADDING_ALIGNMENT) & ~ PADDING_ALIGNMENT);;
+static inline size_t _get_block_size(size_t raw_size) { 
+    size_t raw_block_size = raw_size + 2*sizeof(size_t);
+    return (raw_block_size + PADDING_ALIGNMENT) & ~ PADDING_ALIGNMENT;
+}
 
-static inline size_t _pack(size_t block_size, uint8_t alloc) { return (block_size - 2*(sizeof(size_t))) || alloc;};
+static inline size_t _pack(size_t block_size, uint8_t alloc) { return block_size | alloc;}
 
-static inline size_t _get_size(size_t header) { return header & ~PADDING_ALIGNMENT;};
+static inline size_t _get_size(size_t header) { return header & ~PADDING_ALIGNMENT;}
 
-static inline uint8_t _get_alloc(size_t header) { return header & PADDING_ALIGNMENT;};
+static inline uint8_t _get_alloc(size_t header) { return header & 1u;}
 
-static inline void set(size_t * ptr, size_t header) { 
+static inline void _set(size_t * ptr, size_t header) { 
     *ptr = header; //set header
     size_t block_size =  _get_size(header);
-    *(ptr + sizeof(size_t) + block_size) = header; //set footer
+    *(ptr + (block_size/sizeof(size_t)) - 1) = header; //set footer
     return;
 }
 
@@ -29,16 +33,20 @@ void *ma_malloc(size_t size)
     }
     size_t block_size = _get_block_size(size);
     size_t * ptr = sbrk(block_size);
-    if(ptr == -1){
+    if(ptr == (void *)-1){
         return NULL;
     }
-    (void)size;
-    return NULL;
+    _set(ptr, _pack(block_size, 1));
+    return (void *)(ptr + 1); //returning start of user space
 }
 
 void ma_free(void *ptr)
 {
-    (void)ptr;
+    if(ptr == NULL)
+        return;
+    size_t * header = (size_t *)ptr - 1;
+    size_t block_size = _get_size(*header);
+    _set(header, _pack(block_size, 0));
 }
 
 void *ma_realloc(void *ptr, size_t size)
